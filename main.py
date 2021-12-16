b4229fa77fd5fcc101d0a68d7a309c9d7564806b
Aleksey <lexa.shvetsoff@yandex.ru>
2021-12-16 18:04:06 +0800

    Избавился от лишних функций

--- main.py
+++ main.py
@@ -7,13 +7,6 @@ import pandas as pd
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 
-def sort_dict(dict, key_search):
-    result = collections.defaultdict(list)
-    for item in dict:
-        result[item[key_search]].append({'Название': item['Название'], 'Сорт': item['Сорт'], 'Цена': item['Цена'], 'Картинка': item['Картинка'], 'Акция': item['Акция']})
-    return result
-
-
 def get_age_company():
     year_start = datetime.datetime(year=1920, month=1, day=1, hour=0)
     current_year = datetime.datetime.now()
@@ -23,7 +16,9 @@ def get_age_company():
 age_company = get_age_company()
 excel_data_df = pd.read_excel('wine3.xlsx', sheet_name='Лист1', keep_default_na=False).to_dict(orient='records')
 
-sort_data = sort_dict(excel_data_df, 'Категория')
+wine_by_categories = collections.defaultdict(list)
+for wine in excel_data_df:
+    wine_by_categories[wine['Категория']].append(wine)
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -34,7 +29,7 @@ template = env.get_template('template.html')
 
 rendered_page = template.render(
     years=age_company,
-    wines=sort_data
+    wines=wine_by_categories
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
